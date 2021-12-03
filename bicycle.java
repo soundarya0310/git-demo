b7226a1a9dd08b5f21f974bd5443072c84a5d619
Soundarya <soundaryajeni10@gmail.com>
2021-12-03 14:49:05 +0530

    adding bicycle.java to the repo

---
+++ bicycle.java
@@ -0,0 +1,35 @@
+package com.dnapass.training.java.Inheritance;
+
+public class Bicycle {
+		 public int cadence;
+		    public int gear;
+		    public int speed;
+		    
+		        public Bicycle(int startCadence, int startSpeed, int startGear) {
+		        gear = startGear;
+		        cadence = startCadence;
+		        speed = startSpeed;
+		    }
+		        public void setCadence(int newValue) {
+		            cadence = newValue;
+		        }
+		            
+		        public void setGear(int newValue) {
+		            gear = newValue;
+		        }
+		            
+		        public void applyBrake(int decrement) {
+		            speed -= decrement;
+		        }
+		            
+		        public void speedUp(int increment) {
+		            speed += increment;
+		        }
+			public void printDescription() {
+				
+				System.out.println("\nBike is" + "in gear"+this.gear
+						+ "with a cadence of" +this.cadence +"and travelling at a speed of" +this.speed+".");	
+								}
+			
+
+	}
